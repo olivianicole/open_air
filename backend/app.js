b0b66df23c22ffb6ddd53d670de0af31896bb522
olivianicole <onyoung@umich.edu>
2021-02-18 12:34:12 -0500

    error handling

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const isProduction = environment === 'production'; // we want to check the envir
 app.use(morgan('dev')); // connecting the morgan middleware for logging information about requests and responses
 app.use(cookieParser()); // used to parse cookies
 app.use(express.json()); // middleware for parsing JSON bodies of requests with content-type of 'application/json'
-
+const { ValidationError } = require('sequelize');
 // Security Middleware
 if (!isProduction) {
     app.use(cors()); // enable cors only in development environment
@@ -37,6 +37,38 @@ app.use(
     
 const routes = require('./routes');
 app.use(routes); // connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+// check if error is a Sequelize error:
+if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+}
+next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
 
 module.exports = app;
