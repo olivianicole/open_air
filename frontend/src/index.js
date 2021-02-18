cf442cce19d8f5874f7e4bc3155a565a824b5976
olivianicole <onyoung@umich.edu>
2021-02-18 18:22:28 -0500

    frontend setup complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
-if (process.env.NODE_ENV !== 'production') window.store = store;
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 function Root() {
   return (
