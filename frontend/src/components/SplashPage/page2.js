2c0b4c6ffa16af41784699edfc135e04f104c515
olivianicole <onyoung@umich.edu>
2021-02-26 20:10:14 -0500

    Update page2.js

--- frontend/src/components/SplashPage/page2.js
+++ frontend/src/components/SplashPage/page2.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import './SplashPage.css'
 
 export default function Page2 (){
     
     return (
         <div className='page2'>
-            <div className="logo">oa</div>
-            <div className='component_title'>open air is about simple sharing</div>
-            <div className='descriptor_text'></div>
+            <div className='descriptor_text'>open air is about simple sharing</div>
+            <div className='descriptor_text'>Air your thoughts, air your ideas, see what others are putting out in the open.</div>
         </div>
     )
 }
